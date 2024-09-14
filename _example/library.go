f2beb0dd2ffd3bd75957941c0bacd0f14606c290
iami317 <mlh_210@163.com>
2024-09-14 11:33:06 +0800

    init

--- _example/library.go
+++ _example/library.go
@@ -12,10 +12,10 @@ import (
 func main() {
 	options := &runner.Options{
 		//Proxy:    "socks5://192.168.8.109:8888",
-		Host:     goflags.StringSlice([]string{"192.168.32.149"}),
+		Host:     goflags.StringSlice([]string{"192.168.100.149"}),
 		ScanType: runner.SynScan,
-		TopPorts: "full", //100  1000 full
-		//Ports:             "22,8080,8084",
+		//TopPorts: "full", //100  1000 full
+		Ports:     "22,50000,9093,9092,6379,81,8081,8080,8000,50051,50052,50053,50054,50055",
 		Interface: "en0",
 		//OnlyHostDiscovery: true,
 		//ExcludeCDN:   true,
@@ -32,18 +32,18 @@ func main() {
 		Retries:    runner.DefaultRetriesSynScan,
 		Timeout:    runner.DefaultPortTimeoutSynScan,
 		WarmUpTime: 15,
-		//Debug:      true,
-		Verbose: true,
+		Debug:      true,
+		//Verbose: true,
 
 		OnResult: func(hostResult *result.HostResult) {
 			if len(hostResult.Ports) > 0 {
-				fmt.Printf("***ip:%v **** host:%v **** IsCdn:%v **** CdnName:%v *** Port:%v\n", hostResult.IP, hostResult.Host, hostResult.IsCDNIP, hostResult.CdnName, hostResult.Ports[0].String())
+				fmt.Printf("***ip:%v *** Port:%v %v\n", hostResult.IP, hostResult.Ports[0].Port, len(hostResult.Ports))
 			}
 			fmt.Printf("***ip:%v **** host:%v **** IsCdn:%v **** CdnName:%v\n", hostResult.IP, hostResult.Host, hostResult.IsCDNIP, hostResult.CdnName)
 		},
 		OnReceive: func(hostResult *result.HostResult) {
 			if len(hostResult.Ports) > 0 {
-				fmt.Printf("---ip:%v **** host:%v **** IsCdn:%v **** CdnName:%v *** Port:%v\n", hostResult.IP, hostResult.Host, hostResult.IsCDNIP, hostResult.CdnName, hostResult.Ports[0].String())
+				fmt.Printf("---ip:%v ****Port:%v %v\n", hostResult.IP, hostResult.Ports[0].Port, len(hostResult.Ports))
 			} else {
 				fmt.Printf("---ip:%v **** host:%v **** IsCdn:%v **** CdnName:%v\n", hostResult.IP, hostResult.Host, hostResult.IsCDNIP, hostResult.CdnName)
 			}
